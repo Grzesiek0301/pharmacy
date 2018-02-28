9edeccee5d83015df8f62b41be2baa11d7bf9096
Grzesiek0301 <g.urbanski@op.pl>
2018-02-28 12:36:31 +0100

    Added two classes DataBaseCommand and DataBaseConnector. DataBaseConnector will use to connect with DataBase. DataBaseCommand will use to send expresion to Data Base.

---
+++ src/main/java/DataBaseConnector.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DataBaseConnector {
+
+    public static Connection save() {
+
+        try {
+            String DBURL = "jdbc:mysql://localhost:3306/Pharmacy";
+            String DBUSER = "root";
+            String DBPASS = "";
+            String DBDRIVER = "com.mysql.jdbc.Driver";
+            Class.forName(DBDRIVER);
+            Connection connection = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
+            System.out.println("Connected");
+            return connection;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+}
